435adc8b2fc4475d1aea9fff2686aa9c2906336a
Julia Sávochkina <j.savochkina@admitad.com>
2020-05-24 11:24:26 +0300

    removed excess code for main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from parse_orders import parse_orders
 from validate_orders import validate_order
 
 # url = 'https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J'
-# checkers: dict = {
-#     'check_keys': _keys_check,
-#     'check_values': _values_not_null,
-#     'check_uid': _uid_check
-# }
+
 
 raw_data = get_orders()
 for each in parse_orders(raw_data):
