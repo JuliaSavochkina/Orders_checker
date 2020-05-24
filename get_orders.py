b056d94ac5b0e8fcabcb47bf5e26fec8b12c6110
Julia Sávochkina <j.savochkina@admitad.com>
2020-05-24 12:44:17 +0300

    added description to get_orders.py

--- get_orders.py
+++ get_orders.py
@@ -6,6 +6,11 @@ import requests
 
 # url = input('Ссылка на фид: ')
 def get_orders() -> str:
+    """
+    Обращается к xml файлу и забирает данные.
+    :param url: адрес файла
+    :return: все заказы из файла
+    """
     try:
         r = requests.get('https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J')
         return r.text
