6cc28d72e16285375e81fd9f376a47fb3e52cb6c
Julia Sávochkina <j.savochkina@admitad.com>
2020-05-27 20:54:49 +0300

    prettify get_orders.py

--- get_orders.py
+++ get_orders.py
@@ -1,23 +1,20 @@
 import requests
-
-"""TODO:
-подумать о переносе ввода фида"""
+from export_csv import export_to_csv
 
 
-# url = input('Ссылка на фид: ')
-def get_orders() -> str:
+def get_orders(url: str) -> str:
     """
     Обращается к xml файлу и забирает данные.
     :param url: адрес файла
     :return: все заказы из файла
     """
     try:
-        r = requests.get('https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J')
+        r = requests.get(url)
         return r.text
     except requests.exceptions.RequestException as error:
-        print(error)
+        export_to_csv([f'{error}'])
 
 
 if __name__ == '__main__':
-    # url = 'https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J'
-    print(get_orders)
+    url = 'https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J'
+    print(get_orders(url))
