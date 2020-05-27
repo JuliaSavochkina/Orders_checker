91be5dd1b3851f180b91f391904a2f679cceb868
Julia Sávochkina <j.savochkina@admitad.com>
2020-05-27 21:27:11 +0300

    deleted url from main.py

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ from thread_orders import thread_orders
 
 url = input('Ссылка на фид: ')
 thread_orders(url)
-
-# url = 'https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J'
