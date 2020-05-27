56c938d6a29ccd39771075f264df4738bad63f82
Julia Sávochkina <j.savochkina@admitad.com>
2020-05-27 20:55:07 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from get_orders import get_orders
-from parse_orders import parse_orders
-from validate_orders import validate_order
+from thread_orders import thread_orders
+
+
+url = input('Ссылка на фид: ')
+thread_orders(url)
 
 # url = 'https://mytoys.advcake.com/export/admitad?pass=q8q77yzh4IGPYkiHjgGcvBGdbF3Jbh3J'
-
-
-raw_data = get_orders()
-for each in parse_orders(raw_data):
-    validate_order(each)
