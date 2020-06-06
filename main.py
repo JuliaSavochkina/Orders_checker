e396ef25c4627d246ce990d8ac0a7babb203f9c9
Julia Sávochkina <j.savochkina@admitad.com>
2020-06-06 16:58:45 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from thread_orders import thread_orders
 
 
-url = input('Ссылка на фид: ')
-thread_orders(url)
+def main():
+    url = input('Ссылка на фид: ')
+    thread_orders(url)
+
+
+if __name__ == "__main__":
+    main()
