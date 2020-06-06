6cf60846ec06d0471c41b7b6af87cdca5f53c510
Julia Sávochkina <j.savochkina@admitad.com>
2020-06-06 17:53:06 +0300

    fixed typo in export_csv.py

--- export_csv.py
+++ export_csv.py
@@ -1,12 +1,12 @@
 import csv
 
 
-def export_to_csv(massage: list):
+def export_to_csv(message: list):
     with open('error_log.csv', "a", newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(massage)
+        writer.writerow(message)
 
 
 if __name__ == '__main__':
-    error_massage = ["No key 'uid' for {'order_id': 'myToys.ru-121513935', 'tariff_code': '1'}"]
-    export_to_csv(error_massage)
+    error_message = ["No key 'uid' for {'order_id': 'myToys.ru-121513935', 'tariff_code': '1'}"]
+    export_to_csv(error_message)
